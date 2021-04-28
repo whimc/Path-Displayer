e0d3f4abc4b433de07af68682e5b90ae5f06ebc9
Jack Henhapl <jackah2@illinois.edu>
2021-04-28 16:36:22 -0500

    Rewriting as functional component

--- src/components/PathImage.js
+++ src/components/PathImage.js
@@ -3,26 +3,18 @@ import React from 'react';
 import Alert from 'react-bootstrap/Alert';
 import { GetWorldFromImageName } from '../helpers';
 
-class PathImage extends React.Component {
-    render() {
-        var worldName = GetWorldFromImageName(this.props.title)
-        return (
-            <Alert variant="primary">
-                <Alert.Heading>{worldName}</Alert.Heading>
-                <a
-                    href={this.props.link}
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    
-                    <img
-                        width='100%'
-                        src={this.props.link}
-                        alt={this.props.title}>
-                    </img>
-                </a>
-            </Alert>
-        )
-    }
-}
+
+const PathImage = ({ title, link }) => (
+	<Alert variant="primary">
+		<Alert.Heading>{GetWorldFromImageName(title)}</Alert.Heading>
+		<a
+			href={link}
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<img width='100%' src={link} alt={title} />
+		</a>
+	</Alert>
+);
 
 export default PathImage;
